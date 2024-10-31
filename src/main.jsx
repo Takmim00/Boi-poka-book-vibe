5a6a8b3b9210b1b6d1697fb251333635131200ac
Takmim00 <takmimm@gmail.com>
2024-10-31 19:10:19 +0600

    navbar and footer added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,25 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./Components/Root/Root";
+import ErrorPage from "./Components/ErrorPage/Errorpage";
+import Home from "./Components/Home/Home";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children : [
+      {
+        path:'/',
+        element: <Home></Home>
+      },
+      {
+        path: 'dashboard',
+        element:<Dashboard></Dashboard>
+      }
+    ]
   },
 ]);
 
